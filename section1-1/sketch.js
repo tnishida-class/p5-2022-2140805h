b2ff46de25cfac3e73da3cf655b36bcad4ad579e
2140805h <2140805h@gsuite.kobe-u.ac.jp>
2022-10-04 14:57:06 +0900

    Change section1-1

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -5,11 +5,14 @@ function setup() {
 function draw() {
   background(0);
   strokeWeight(4);
-  fill(94, 185, 84);
+  fill(125, 15, 130);
   triangle(12,120,116,120,116,10);
-  fill(178, 100, 162);
+  fill(237, 161, 187);
   triangle(0,108,106,108,106,0);
-  fill(255);
+  strokeWeight(0)
+  fill(0)
+  triangle(8,105,103,105,103,8)
+  fill(237,161,187);
   textSize(32);
   textFont("serif");
   text("46", 68, 100);
