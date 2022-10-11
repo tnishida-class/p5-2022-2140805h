6faca90955f6e31b309b19cb5a63be3844cb5705
2140805h <2140805h@gsuite.kobe-u.ac.jp>
2022-10-11 12:19:38 +0900

    Update sketch.js

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -6,6 +6,20 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      if((i + j) % 2 == 0){
+      fill(255);
+      rect(size * i, size * j, size * (i + 1), size * (j + 1));
+    }
+    else{
+      fill(160);
+      rect(size * i, size * j, size * (i + 1), size * (j + 1));
+    }
+    
+    if((i + j) % 2 == 1){
+      fill(255, 0, 0);
+      ellipse(size * i, size * j, size % 2)
+    };
+    
     }
   }
 }
