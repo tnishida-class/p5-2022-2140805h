51d8269addeb466261cc1083112ea20893710511
2140805h <2140805h@gsuite.kobe-u.ac.jp>
2022-10-18 12:15:42 +0900

    Change section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,6 +1,7 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let size;
 
 function setup(){
   createCanvas(200, 200);
@@ -11,6 +12,37 @@ function setup(){
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
-  // BLANK[1]
+  
+  // let x;
+  // let y;
+
+  if(keyIsDown(" ".charCodeAt(0))){
+
+  if(count < 25){
+    size = 2 * count;
+  } 
+    else if(count > 25 && count < 50){
+    size = 100 - (2 * count);
+    }
+    else if(count > 50 && count < 75){
+      size = - 100 + (2 * count);
+    }
+    else if(count > 75){
+      size = 200 - (2 * count);
+    }
+                     
+  }
+  
+  else{
+
+    if(count < 50){
+      size = count;
+    } 
+    else{size = 100 - (1 * count);}
+
+  }
+
   ellipse(width / 2, height / 2, size);
+
+  
 }
