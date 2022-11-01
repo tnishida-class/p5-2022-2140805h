064d47cc302f6a767b72a60081cf82725a5af286
2140805h <2140805h@gsuite.kobe-u.ac.jp>
2022-11-01 12:13:44 +0900

    Change section5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -4,15 +4,26 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  textSize(32);
+  textFont('Georgia');
+  balloon("I love keyakizaka46", 50, 50, 165, 5, 165);
 }
 
-function balloon(t){
+function balloon(t, x, y, r, g, b){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
-  text(t, p, h + p);
+  // p = padding 文字と図形の隙間、margin = 図形と外の隙間
+  
+  fill(r, g, b);
+  noStroke();
+  rect(x, y, w + p * 2, h + p * 2);
+  
+  triangle(x + (w + p * 2) * 9 / 10 , y + h + p * 2, 
+            x + w + p * 2, y + h + p * 2, 
+            x + (w + p * 2) + 5,  y + ( h + p * 2) * 1.5)
+
+  fill(250, 205, 250);
+  text(t, x + p, y + (h + p * 2) * 2 / 3);
+  // console.log(textDescent());
 }
